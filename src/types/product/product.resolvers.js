5cb2039bea1137cce446fcfaffd3bf30005af94d
wk32 <wk.liang.84@gmail.com>
2019-06-26 22:48:31 +0800

    solution for lesson2

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,9 +9,65 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const newProduct = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+
+  return Product.create({ ...args.input, createdBy: ctx.user._id })
+}
+
+const product = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+  const currProduct = Product.findById(args.id)
+    .lean()
+    .exec()
+  console.log(currProduct)
+  return currProduct
+}
+
+const products = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+  return Product.find({})
+}
+
+const updateProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role != roles.admin) {
+    throw new AuthenticationError()
+  }
+  const update = args.input
+  return Product.findByIdAndUpdate(args.id, update, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role != roles.admin) {
+    throw new AuthenticationError()
+  }
+  return Product.findOneAndDelete(args.id)
+    .lean()
+    .exec()
+}
+
+const __resolveType = product => {
+  productsTypeMatcher.forEach(val => console.log('val is ', val))
+}
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    product,
+    products
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
     __resolveType(product) {}
   }
